433819ca4e21bd5b81392ba68938b752c3491ab3
Jan Karstens <jan.karstens@blue-yonder.com>
2014-02-11 22:45:46 +0100

    some more steps towards a running Travis CI build

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-sqlalchemy_exasol
-=================
+h1. sqlalchemy_exasol
 
-SQLAlchemy dialect for EXASol
+This is an SQLAlchemy dialect for the EXASOL database. To learn more about:
+
+* EXASOL please visit www.exasol.com
+* SQLAlchemy please visit www.sqlalchemy.org
--- setup.py
+++ setup.py
@@ -10,15 +10,33 @@ do not forget to document your changes in CHANGES.md
 :authors: Blue Yonder GmbH
 :date: 2014/02/11
 """
+import os
+from setuptools import setup
+
 VERSION = '0.7.0'
 
-from setuptools import setup
+## Get long_description from README.md:
+here = os.path.dirname(os.path.abspath(__file__))
+long_description = ''
+with open(os.path.join(here, 'README.md')) as f:
+    long_description = f.read().strip()
 
 setup(
     name='sqlaexasol',
     version=VERSION,
+    license='License :: OSI Approved :: BSD License',
+    url='https://github.com/BY-jk/sqlalchemy_exasol',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Database'
+    ],
     description='EXASOL dialect for SQLAlchemy',
-    author='Blue Yonder',
+    long_description=long_description,
+    author='Blue Yonder GmbH',
     packages=['exa'],
     install_requires=["SQLAlchemy>=0.8.2, <0.9", "pyodbc>=3.0.6"],
     tests_require=['nose>=0.11', 'coverage>=3.7.1', 'mock>=1.0.1'],

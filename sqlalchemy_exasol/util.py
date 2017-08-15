10cc39feaf7fc278c2ea9df75ce7e4d07415cf86
Peter Hoffmann <ph@peter-hoffmann.com>
2017-08-15 19:35:55 +0200

    fix encoding

--- sqlalchemy_exasol/util.py
+++ sqlalchemy_exasol/util.py
@@ -1,4 +1,5 @@
 import datetime
+import six
 
 from sqlalchemy_exasol.pyodbc import EXADialect_pyodbc
 from sqlalchemy_exasol import base
@@ -7,11 +8,6 @@ def raw_sql(query):
     """
     Converts an SQLAlchemy object to a single raw SQL string. Note
     that this function currently only supports Exasol queries!
-
-    .. note::
-
-        If you want to pass a string to a sqlalchemy connection please use
-        :func:`unicode_sql` to handle unicode values correctly.
 
     :param query: An SQLAlchemy query object
 
@@ -31,21 +27,18 @@ def raw_sql(query):
         def render_literal_value(self, value, type_):
             if value is None:
                 return 'NULL'
-            elif isinstance(value, str):
-                return u"'{value}'".format(value=unicode(value, encoding='utf-8'))
-            elif isinstance(value, unicode):
+            elif isinstance(value, six.binary_type):
+                return u"'{value}'".format(value=value.decode('utf-8'))
+            elif isinstance(value, six.text_type):
                 return u"'{value}'".format(value=value)
             elif type(value) is datetime.date:
                 return "to_date('{value}', 'YYYY-MM-DD')"\
                     .format(value=value.strftime('%Y-%m-%d'))
             elif type(value) is datetime.datetime:
-                return "to_timestamp('{value}', 'YYYY-MM-DD HH24:MI:SS.FF6')"\
+                return u"to_timestamp('{value}', 'YYYY-MM-DD HH24:MI:SS.FF6')"\
                     .format(value=value.strftime('%Y-%m-%d %H:%M:%S.%f'))
             else:
-                return str(value)
+                return u"{}".format(value)
 
     compiler = LiteralCompiler(dialect, query)
-    raw_sql_string = compiler.string
-    raw_sql_string = raw_sql_string.encode('UTF-8')
-
-    return raw_sql_string
+    return compiler.string

f337958ceccfe855617f0bbeae3b4419de8db278
Nicola Coretti <nicola.coretti@exasol.com>
2022-03-29 12:13:01 +0200

    Supress spurious warning from pytest
    see also https://github.dev/sqlalchemy/sqlalchemy/blob/4fd4e4b0d6d20586614a18c3365476184d1a6a70/README.dialects.rst#L105-L106

--- test/conftest.py
+++ test/conftest.py
@@ -1,6 +1,10 @@
 from sqlalchemy.dialects import registry
+import pytest
 
 registry.register("exa.pyodbc", "sqlalchemy_exasol.pyodbc", "EXADialect_pyodbc")
 registry.register("exa.turbodbc", "sqlalchemy_exasol.turbodbc", "EXADialect_turbodbc")
+
+# Suppress spurious warning from pytest
+pytest.register_assert_rewrite("sqlalchemy.testing.assertions")
 
 from sqlalchemy.testing.plugin.pytestplugin import *

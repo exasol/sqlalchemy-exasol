cd2d603cc7ab8c529d119e3b4a3d9ebe1886c42c
Nicola Coretti <nicola.coretti@exasol.com>
2023-05-12 12:41:48 +0200

    Fix typo

--- test/integration/regression/test_regression_bug335.py
+++ test/integration/regression/test_regression_bug335.py
@@ -74,7 +74,7 @@ def test_lastrowid_does_not_create_extra_commit(
         conn = engine.connect()
         trans = conn.begin()
 
-        # inserting without and explicit id will trigger get_lastrowid
+        # insert without and explicit id will trigger get_lastrowid
         insert_statement = insert(table).values(name="Gandalf")
         conn.execute(insert_statement)
         trans.rollback()

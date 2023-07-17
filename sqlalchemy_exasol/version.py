62c7072f3e5ba9e2be47d682ac4a7b95fd1bd1e7
Nicola Coretti <nicola.coretti@exasol.com>
2023-07-17 13:14:39 +0200

    Prepare release 4.6.0

--- sqlalchemy_exasol/version.py
+++ sqlalchemy_exasol/version.py
@@ -3,7 +3,7 @@
 # If you need to change the version, do so in the project.toml, e.g. by using `poetry version X.Y.Z`.
 
 MAJOR = 4
-MINOR = 5
-PATCH = 1
+MINOR = 6
+PATCH = 0
 
 VERSION = f"{MAJOR}.{MINOR}.{PATCH}"

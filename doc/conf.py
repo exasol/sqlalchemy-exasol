a48de4eea034e9501b0564de819c0da5ae5c74c9
Nicola Coretti <nicola.coretti@exasol.com>
2024-01-24 09:24:36 +0100

    Fix formatting of sphinx config

--- doc/conf.py
+++ doc/conf.py
@@ -33,10 +33,7 @@ release = VERSION
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    "sphinx_copybutton",
-    'sphinx.ext.intersphinx'
-]
+extensions = ["sphinx_copybutton", "sphinx.ext.intersphinx"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]

fde58e8f6f2ba3575946aebe1fda9fc54bff5485
Nicola Coretti <nicola.coretti@exasol.com>
2024-01-31 10:10:55 +0100

    Add warnings module and deprecation types

---
+++ sqlalchemy_exasol/warnings.py
@@ -0,0 +1,9 @@
+from warnings import UserWarning, DeprecationWarning
+
+
+class SqlaExasolWarning(UserWarning):
+    """Base class for all warnings emited by sqlalchemy_exasol."""
+
+
+class SqlaExasolDeprecationWarning(SqlaExasolWarning, DeprecationWarning):
+    """Warning class for features that will be removed in future versions."""

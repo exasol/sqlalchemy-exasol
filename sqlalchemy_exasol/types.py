122dce4f41b357fe9556b4108371e5d4017509fa
Ariel Schulz <ariel.schulz@exasol.com>
2025-10-14 09:28:39 +0200

    Clean up ExaDecimal

--- sqlalchemy_exasol/types.py
+++ sqlalchemy_exasol/types.py
@@ -7,19 +7,24 @@ class ExaDecimal(sqltypes.DECIMAL):
     def bind_processor(self, dialect):
         return super().bind_processor(dialect)
 
-    def result_processor(self, dialect, coltype):
-        if not self.asdecimal:
-            return lambda value: None if value is None else float(value)
-
+    def to_decimal(self, value):
         fstring = "%%.%df" % self._effective_decimal_return_scale
 
-        def to_decimal(value):
-            if value is None:
-                return None
-            if isinstance(value, decimal.Decimal):
-                return value
-            if isinstance(value, float):
-                return decimal.Decimal(fstring % value)
-            return decimal.Decimal(value)
+        if value is None:
+            return None
+        if isinstance(value, decimal.Decimal):
+            return value
+        if isinstance(value, float):
+            return decimal.Decimal(fstring % value)
+        return decimal.Decimal(value)
 
-        return to_decimal
+    @staticmethod
+    def handle_not_as_decimal(value):
+        if value is None:
+            return None
+        return float(value)
+
+    def result_processor(self, dialect, coltype):
+        if self.asdecimal:
+            return self.to_decimal
+        return self.handle_not_as_decimal

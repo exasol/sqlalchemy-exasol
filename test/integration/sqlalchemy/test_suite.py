4cec7645d3e7de74b5f54ca991cc2916eb0f1584
Ariel Schulz <ariel.schulz@exasol.com>
2025-11-11 09:34:44 +0100

    Finish moving skips to xfails

--- test/integration/sqlalchemy/test_suite.py
+++ test/integration/sqlalchemy/test_suite.py
@@ -48,6 +48,13 @@ class XfailRationale(str, Enum):
         Manual indexes are not recommended within the Exasol DB.
         """
     )
+    QUOTING = cleandoc(
+        """This suite was added to SQLAlchemy 1.3.19 on July 2020 to address
+        issues in other dialects related to object names that contain quotes
+        and double quotes. Since this feature is not relevant to the
+        Exasol dialect, the entire suite is set to xfail. For further info, see:
+        https://github.com/sqlalchemy/sqlalchemy/issues/5456"""
+    )
     SELECT_LIST = cleandoc(
         """Exasol does not allow EXISTS or IN predicates as part of the select list."""
     )
@@ -427,50 +434,9 @@ class ExceptionTest(_ExceptionTest):
             assert_raises(exc.IntegrityError, conn.execute, insert)
 
 
+@pytest.mark.xfail(reason=XfailRationale.QUOTING.value, strict=True)
 class QuotedNameArgumentTest(_QuotedNameArgumentTest):
-    RATIONAL = cleandoc(
-        """This suite was added to SQLAlchemy 1.3.19 on July 2020 to address
-        issues in other dialects related to object names that contain quotes
-        and double quotes. Since this feature is not relevant to the
-        Exasol dialect, the entire suite will be skipped. More info on fix:
-        https://github.com/sqlalchemy/sqlalchemy/issues/5456"""
-    )
-
-    @pytest.mark.skip(reason=RATIONAL)
-    def test_get_table_options(self, name):
-        return
-
-    @pytest.mark.skip(reason=RATIONAL)
-    def test_get_view_definition(self, name):
-        return
-
-    @pytest.mark.skip(reason=RATIONAL)
-    def test_get_columns(self, name):
-        return
-
-    @pytest.mark.skip(reason=RATIONAL)
-    def test_get_pk_constraint(self, name):
-        return
-
-    @pytest.mark.skip(reason=RATIONAL)
-    def test_get_foreign_keys(self, name):
-        return
-
-    @pytest.mark.skip(reason=RATIONAL)
-    def test_get_indexes(self, name):
-        return
-
-    @pytest.mark.skip(reason=RATIONAL)
-    def test_get_unique_constraints(self, name):
-        return
-
-    @pytest.mark.skip(reason=RATIONAL)
-    def test_get_table_comment(self, name):
-        return
-
-    @pytest.mark.skip(reason=RATIONAL)
-    def test_get_check_constraints(self, name):
-        return
+    pass
 
 
 class NumericTest(_NumericTest):

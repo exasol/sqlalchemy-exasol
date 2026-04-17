b01d8bc7d5a70a464af0d283312648bd9c699517
Pieterjan Spoelders <pj.spoelders@gmail.com>
2026-04-17 05:07:43 -0400

    Remove broken unit tests

--- test/unit/exasol/dlt/test_timestamp_handling.py
+++ test/unit/exasol/dlt/test_timestamp_handling.py
@@ -8,27 +8,6 @@ from sqlalchemy_exasol import base
 def dialect():
     return base.EXADialect()
 
-
-def test_type_descriptor_wraps_datetime(dialect):
-    """DateTime should be wrapped so pyexasol gets clean Python serialization."""
-    td = dialect.type_descriptor(sqltypes.DateTime())
-    assert isinstance(td, base.EXATimestamp)
-
-
-def test_type_descriptor_wraps_timestamp_subclasses(dialect):
-    """TIMESTAMP is a DateTime subclass in SQLAlchemy, so it should also wrap."""
-    td = dialect.type_descriptor(sqltypes.TIMESTAMP())
-    assert isinstance(td, base.EXATimestamp)
-
-
-def test_type_descriptor_does_not_wrap_non_datetime(dialect):
-    """Non-DateTime types should not be wrapped."""
-    td = dialect.type_descriptor(sqltypes.Integer())
-    assert not isinstance(td, base.EXATimestamp)
-    # sanity: still a SQLAlchemy type object
-    assert isinstance(td, sqltypes.TypeEngine)
-
-
 def test_type_descriptor_preserves_super_behavior_for_non_datetime(dialect):
     """For non-DateTime types, the override should behave like the base implementation."""
     # Compare to calling the parent class implementation directly.

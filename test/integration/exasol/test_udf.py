17dd3d733a78f2931bdb6f9c8cb0044b8c06840a
ckunki <christoph.kuhnke@exasol.com>
2026-08-14 13:24:35 +0200

    Fixed itest

--- test/integration/exasol/test_udf.py
+++ test/integration/exasol/test_udf.py
@@ -4,6 +4,10 @@ from sqlalchemy import (
     create_engine,
     text,
 )
+from sqlalchemy.schema import (
+    CreateSchema,
+    DropSchema,
+)
 from sqlalchemy.testing import (
     config,
     fixtures,
@@ -14,18 +18,20 @@ class Udf(fixtures.TestBase):
     @classmethod
     def setup_class(cls):
         cls.schema = "test"
-
-    # @classmethod
-    # def teardown_class(cls):
-    #     with config.db.begin() as con:
-    #         con.execute(text("DROP SCRIPT UDF"))
+        with config.db.begin() as conn:
+            conn.execute(CreateSchema(cls.schema))
+            
+    @classmethod
+    def teardown_class(cls):
+        with config.db.begin() as conn:
+            conn.execute(DropSchema(cls.schema, cascade=True))
 
     def test_udf(self):
         engine = create_engine(config.db.url)
-        UDF = cleandoc("""
+        UDF = cleandoc(f"""
             --/
             CREATE OR REPLACE PYTHON3 SCALAR SCRIPT
-              UDF(
+              "{self.schema}".UDF(
               "a" VARCHAR(200)
             ) EMITS (
               "result" VARCHAR(2000)
@@ -36,5 +42,5 @@ class Udf(fixtures.TestBase):
         """)
         with engine.connect() as con:
             con.execute(text(UDF))
-            res = con.execute(text("SELECT UDF('abc')")).fetchone()
+            res = con.execute(text(f"SELECT "{self.schema}".UDF('abc')")).fetchone()
         assert res[0] == "Input: abc"
